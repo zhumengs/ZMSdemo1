5d3b80d0285f5c65d728b41f44b495cd81250e00
zms <714816525@qq.com>
2018-11-04 22:55:19 +0800

    20181104

---
+++ app/src/main/java/com/example/administrator/zms/recyclerview/LinearRecyclerViewActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.zms.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearRecyclerViewActivity extends AppCompatActivity {
+
+    /*private RecyclerView mRvMain;*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_recycler_view);
+        /*mRvMain = (RecyclerView)findViewById(R.id.rv_main);
+        mRvMain.setLayoutManager(new LinearLayoutManager(LinearRecyclerViewActivity.this));*/
+    }
+}
---
+++ app/src/main/java/com/example/administrator/zms/recyclerview/RecyclerViewActivity.java
@@ -0,0 +1,26 @@
+package com.example.administrator.zms.recyclerview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    private Button mBtnLinear;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+        mBtnLinear = (Button)findViewById(R.id.btn_linear);
+        mBtnLinear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(RecyclerViewActivity.this,LinearRecyclerViewActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
