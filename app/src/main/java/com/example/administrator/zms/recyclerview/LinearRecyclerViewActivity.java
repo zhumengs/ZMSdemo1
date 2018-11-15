9cefa03e2d87888d861b1a34d3d32c68892ba10b
zms <714816525@qq.com>
2018-11-15 23:48:24 +0800

    20181115

--- app/src/main/java/com/example/administrator/zms/recyclerview/LinearRecyclerViewActivity.java
+++ app/src/main/java/com/example/administrator/zms/recyclerview/LinearRecyclerViewActivity.java
@@ -1,9 +1,12 @@
 package com.example.administrator.zms.recyclerview;
 
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.administrator.zms.R;
 
@@ -17,6 +20,20 @@ public class LinearRecyclerViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_linear_recycler_view);
         mRvMain = (RecyclerView)findViewById(R.id.rv_main);
         mRvMain.setLayoutManager(new LinearLayoutManager(LinearRecyclerViewActivity.this));
-        mRvMain.setAdapter(new LinearAdapter(LinearRecyclerViewActivity.this));
+        mRvMain.addItemDecoration(new MyDecoration());
+        mRvMain.setAdapter(new LinearAdapter(LinearRecyclerViewActivity.this, new LinearAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int pos){
+                Toast.makeText(LinearRecyclerViewActivity.this,"click..."+pos,Toast.LENGTH_SHORT).show();
+            }
+        }));
     }
+
+    class MyDecoration extends RecyclerView.ItemDecoration{
+        @Override
+        public  void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state){
+            super.getItemOffsets(outRect,view,parent,state);
+            outRect.set(0,0,0,getResources().getDimensionPixelOffset(R.dimen.dividerHeight));
+        }
+    }//在每一个元素的下面添加1dp的间距
 }
