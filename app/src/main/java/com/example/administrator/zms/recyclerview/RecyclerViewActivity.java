53eae7774c11e75e8c6e6469a7905d380b07bda7
zms <714816525@qq.com>
2018-11-19 23:02:36 +0800

    20181119

--- app/src/main/java/com/example/administrator/zms/recyclerview/RecyclerViewActivity.java
+++ app/src/main/java/com/example/administrator/zms/recyclerview/RecyclerViewActivity.java
@@ -9,16 +9,24 @@ import com.example.administrator.zms.R;
 
 public class RecyclerViewActivity extends AppCompatActivity {
 
-    private Button mBtnLinear;
+    private Button mBtnLinear,mBtnHor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
         mBtnLinear = (Button)findViewById(R.id.btn_linear);
+        mBtnHor = findViewById(R.id.btn_hor);
         mBtnLinear.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(RecyclerViewActivity.this,LinearRecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        mBtnHor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecyclerViewActivity.this,HorRecyclerViewActivity.class);
                 startActivity(intent);
 
             }
