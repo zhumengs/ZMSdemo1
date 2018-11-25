523fbecedc015b872d68f9b81fc50c52c8cbf00b
zms <714816525@qq.com>
2018-11-25 23:14:13 +0800

    20181125

--- app/src/main/java/com/example/administrator/zms/recyclerview/LinearAdapter.java
+++ app/src/main/java/com/example/administrator/zms/recyclerview/LinearAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +17,7 @@ import java.util.List;
  * Created by Administrator on 2018/11/8 0008.
  */
 
-public class LinearAdapter extends RecyclerView.Adapter<LinearAdapter.LinearViewHolder> {
+public class LinearAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context mContext;
     private OnItemClickListener mListener;
@@ -29,20 +30,38 @@ public class LinearAdapter extends RecyclerView.Adapter<LinearAdapter.LinearView
     }
 
     @Override
-    public LinearAdapter.LinearViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == 0) {
+            return new LinearViewHolder(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item, parent));
+        }else{
+            return new LinearViewHolder2(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item, parent));
+        }
 
-        return new LinearViewHolder(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item,parent));
     }
 
     @Override
-    public void onBindViewHolder(LinearAdapter.LinearViewHolder holder,final int position){
-        holder.textView.setText("Hello");
+    public void onBindViewHolder(RecyclerView.ViewHolder holder,final int position){
+        if(getItemViewType(position)==0){
+            ((LinearViewHolder)holder).textView.setText("Hello");
+        }else {
+            ((LinearViewHolder2) holder).textView.setText("Hello1");
+        }
         holder.itemView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 mListener.onClick(position);
             }
         });
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(position % 2 ==0){
+            return 0;
+        }else{
+            return 1;
+        }
     }
 
     @Override
@@ -57,6 +76,18 @@ public class LinearAdapter extends RecyclerView.Adapter<LinearAdapter.LinearView
         public LinearViewHolder(View itemView){
             super(itemView);
             textView = itemView.findViewById(R.id.tv_title);
+        }
+    }
+
+    class LinearViewHolder2 extends RecyclerView.ViewHolder{
+
+        private TextView textView;
+        private ImageView imageView;
+
+        public LinearViewHolder2(View itemView){
+            super(itemView);
+            textView = itemView.findViewById(R.id.tv_title);
+            imageView = (ImageView)itemView.findViewById(R.id.iv_image);
         }
     }
 
